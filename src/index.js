a82e3489de4c2d5f6bd455413ea7857b51142236
Yu357 <forestgreen14689@gmail.com>
2022-04-28 12:05:22 +0900

    clean

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-import './styles/index.css';
-import Header from './components/Header';
+import './styles/index.css'
+import Header from './components/Header'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <Header/>
   </React.StrictMode>
-);
+)
