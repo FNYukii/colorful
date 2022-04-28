86879cb65feb214b84bef8ed08fb4e020a083ff1
Yu357 <forestgreen14689@gmail.com>
2022-04-28 12:15:51 +0900

    feat: Footerを表示

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 
 import './styles/index.css'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <Header/>
+    <Footer/>
   </React.StrictMode>
 )
