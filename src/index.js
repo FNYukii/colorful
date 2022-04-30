99c9bf9b32a520647fbf03af7d1e644d8890eb51
Yu357 <forestgreen14689@gmail.com>
2022-04-30 12:47:16 +0900

    clean: 主要三画面をcomponentsフォルダへ移動

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import ReactDOM from 'react-dom/client'
 import './styles/index.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import Top from './screens/Top'
-import Products from './screens/Products'
-import Contact from './screens/Contact'
+import Top from './components/Top'
+import Products from './components/Products'
+import Contact from './components/Contact'
 
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
