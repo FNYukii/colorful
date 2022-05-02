7d93420cf2d8a984b1a4a9527ac0b8ba4edf006d
Yu357 <forestgreen14689@gmail.com>
2022-05-02 11:40:31 +0900

    feat

--- src/components/TextSection.js
+++ src/components/TextSection.js
@@ -2,20 +2,33 @@ import '../styles/textSection.css'
 
 function TextSection(props) {
 
-  let imageContainer = '';
-  if (props.isLarge) {
-    imageContainer = 'large-container';
-  } else {
-    imageContainer = 'small-container';
+  function imageContainer() {
+    let imageContainer = '';
+    if (props.isLarge) {
+      imageContainer = 'large-container';
+    } else {
+      imageContainer = 'small-container';
+    }
+    return imageContainer
+  }
+
+  function titleElement() {
+    let titleElement 
+    if (props.isLarge) {
+      titleElement = <h1>{props.title}</h1>
+    } else {
+      titleElement = <h2>{props.title}</h2>
+    }
+    return titleElement
   }
   
   return (
     <div className="section">
-      <div className={imageContainer}>
+      <div className={imageContainer()}>
         <img src={props.photo} className="trigger-to-popup-modal" alt=''/>
       </div>
       <div className="small-container">
-        <h2>{props.title}</h2>
+        {titleElement()}
         <p style={{whiteSpace: 'pre-line'}}>{props.text}</p>
       </div>
     </div>
